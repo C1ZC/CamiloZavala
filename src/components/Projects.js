fc4e5a3d17b73d69447766d1f218cbf0230fd536
C1ZC <camilo.zavala.c@gmail.com>
2024-07-02 03:27:27 -0400

    agregar

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,6 +6,7 @@ import 'react-multi-carousel/lib/styles.css';
 import projImg1 from '../assets/img/project-img1.png';
 import projImg2 from '../assets/img/project-img2.png';
 import projImg3 from '../assets/img/project-img3.png';
+import projImg4 from '../assets/img/project-img4.png';
 import colorSharp2 from '../assets/img/color-sharp2.png';
 
 export const Projects = () => {
@@ -36,6 +37,38 @@ export const Projects = () => {
       repoUrl: "https://github.com/C1ZC/Login-API-Google", // URL del repositorio
       appUrl: "https://camilozavala.com/Login-API-Google", // URL de la aplicación
       skills: ['HTML', 'CSS','JavaScript', 'PHP','Bootstrap','Cpanel','Google API OAuth 2.0']
+    },
+    {
+      title: "Camilo Zavala HERPM13051 S9",
+      description: "Aplicación Android desarrollada en Kotlin, incluye una pantalla inicial con animación y menú, un listado de vendedores y una funcionalidad de reproducción de audio al seleccionar un vendedor específico. También permite capturar y guardar fotos y visualizar un gráfico de ventas.",
+      imgUrl: projImg4,
+      repoUrl: "https://github.com/C1ZC/camilo_zavala_herpm13051_s9", // URL del repositorio
+      appUrl: "https://youtu.be/sz3H5cJS4yE", // URL de la aplicación
+      skills: ['Kotlin', 'Android Studio','Java','SQLite','API']
+    },
+    {
+      title: "Camilo Zavala HERPM13051 S5",
+      description: "Aplicación Android desarrollada en Kotlin, incluye una pantalla inicial con animación y menú, un listado de vendedores y una funcionalidad de reproducción de audio al seleccionar un vendedor específico. También permite capturar y guardar fotos y visualizar un gráfico de ventas.",
+      imgUrl: projImg4,
+      repoUrl: "https://github.com/C1ZC/camilo_zavala_HERPM13051_s5", // URL del repositorio
+      appUrl: "https://youtu.be/O6qlC0ZfX1Y?si=hPHghqF4CtwiJRIL", // URL de la aplicación
+      skills: ['Kotlin', 'Android Studio','Java']
+    },
+    {
+      title: "Camilo Zavala HERPM13051 S6",
+      description: "Aplicación Android desarrollada en Kotlin, la aplicación permite registrar, modificar y listar infracciones en locales comerciales. Utiliza SQLite para el almacenamiento persistente de datos y facilita la compartición de información con otras aplicaciones.",
+      imgUrl: projImg4,
+      repoUrl: "https://github.com/C1ZC/camilo_zavala_HERPM13051_s6", // URL del repositorio
+      appUrl: "https://www.youtube.com/watch?v=FaLr9l4oWjg", // URL de la aplicación
+      skills: ['Kotlin', 'Android Studio','Java', 'SQLite']
+    },
+    {
+      title: "Camilo Zavala HERPM13051 S7",
+      description: "Aplicación Android desarrollada en Kotlin, la aplicación permite funcionalidades como obtención de días feriados desde una API pública, visualización de los datos en forma de lista, una pantalla de bienvenida (SplashScreen), y un sistema de autenticación que almacena las preferencias del usuario para mantener la sesión iniciada. Además, ofrece la opción de cerrar sesión con un solo clic.",
+      imgUrl: projImg4,
+      repoUrl: "https://github.com/C1ZC/camilo_zavala_HERPM13051_s7", // URL del repositorio
+      appUrl: "https://youtu.be/NxfkyU2xynw", // URL de la aplicación
+      skills: ['Kotlin', 'Android Studio','Java','API','ScreenSplash']
     }
 
   ];
